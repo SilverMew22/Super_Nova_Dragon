63cc1510a8aae7db056d4bc1fcd79c42d5ca97c9
Student <student@v314-ent-006.local>
2019-10-22 15:28:32 -0400

    update

--- Super Nova Dragon (3d)/Assets/SND/SND_Scripts/Nova_mover.cs
+++ Super Nova Dragon (3d)/Assets/SND/SND_Scripts/Nova_mover.cs
@@ -11,11 +11,18 @@ public class Nova_mover : MonoBehaviour
     Animator myAnim;
     AudioSource audioS;
     public AudioClip moveSound;
+
+    Rigidbody myRb;
+    public float multiplier = 10f;
+    //int Prey_Eaten;
+    //public int eatThresh = 3;
     // Start is called before the first frame update
     void Start()
     {
         myAnim = GetComponent<Animator>();
         audioS = GetComponent<AudioSource>();
+
+        myRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -25,11 +32,42 @@ public class Nova_mover : MonoBehaviour
         //myRb.AddForce(new Vector3(0f, 0f, 1f) * multiplier);
         myAnim.SetInteger("state", 0);
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            transform.position += new Vector3(1 * Time.deltaTime * speed, 0, 0);
+            myRb.AddForce(new Vector3(0f, 0f, 1f) * multiplier);
             myAnim.SetInteger("state", 1);
-
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+           myRb.AddForce(new Vector3(-1f, 0f, 0f) * multiplier);
+            myAnim.SetInteger("state", 1);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            myRb.AddForce(new Vector3(0f, 0f, -1f) * multiplier);
+            myAnim.SetInteger("state", 1);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+           myRb.AddForce(new Vector3(1f, 0f, 0f) * multiplier);
+            myAnim.SetInteger("state", 1);
         }
     }
-}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Preditor"))
+        {        
+            //Destroy(gameObject);
+            }
+
+            //if (Input.GetKey(KeyCode.W))
+            //{
+            //    transform.position += new Vector3(1 * Time.deltaTime * speed, 0, 0);
+            //    myAnim.SetInteger("state", 1);
+
+            //}
+        }
+    }
+
+
