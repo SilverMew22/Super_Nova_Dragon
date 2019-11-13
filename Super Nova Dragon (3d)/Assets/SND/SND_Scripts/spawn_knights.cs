46437404a37188345424771d28986941ecb162b9
Mohamed <only1hamada@live.com>
2019-11-13 11:48:43 -0500

    update

--- Super Nova Dragon (3d)/Assets/SND/SND_Scripts/spawn_knights.cs
+++ Super Nova Dragon (3d)/Assets/SND/SND_Scripts/spawn_knights.cs
@@ -7,6 +7,8 @@ public class spawn_knights : MonoBehaviour
     public GameObject clone;
     public Vector3 spawnLocation;
 
+    public Transform parent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,7 @@ public class spawn_knights : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        spawnLocation = parent.position;
         //change if to a time condition (night) and stop spawning once goal it met
         if (Input.GetKeyDown(KeyCode.K))
 
