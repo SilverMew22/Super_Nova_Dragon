bba5c9083623ba0475b36a2863a8d998d2a1fa2f
Shequana <ShequanaGarnett@gmail.com>
2019-12-02 21:31:35 -0500

    update

---
+++ Super Nova Dragon (3d)/Assets/SND/SND_Scripts/_Movement/look_at.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class look_at : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        // Point the object at the world origin (0,0,0)
+        transform.LookAt(Vector3.zero);
+    }
+}
